4f6a523449436c4fe385f4e923ac8e2f48545260
rcy <Rachel@rcy.local>
2013-06-30 16:13:23 +0100

    First commit, generating rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QueensBookings::Application.config.secret_key_base = '81d95e5252164d5ae1cca31df25f910afc2de0528919a5aaf3ac63113615ce9f418e65ba6f6142ec274e1d6473ba221a20037a3f95743624317c66b6584803cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QueensBookings::Application.config.session_store :cookie_store, key: '_QueensBookings_session'
