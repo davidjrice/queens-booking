d60e448c1ae67ccdd2b9c261ad4b466336d6345f
Rachel Rice <rachel.rice@gmail.com>
2013-06-30 17:55:05 +0100

    Added new reservation page, add global flash message, handle create success / fail

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,23 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.all
   end
 
+  def new
+  	@reservation = Reservation.new
+  end
+
+  def create
+  	@reservation = Reservation.new reservation_params
+    if @reservation.save
+    	redirect_to reservations_path, :notice => "Reservation Made"
+    else
+      render :action => :new
+    end
+  end
+
+  protected
+
+  def reservation_params
+  	params.require(:reservation).permit(:start_at, :end_at)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def render_flash
+    output = ""
+    flash.each do |name, msg|
+      if msg.is_a?(String)
+        output << (content_tag :div, msg, :id => "flash_#{name}", :class => "alert")
+      end
+    end
+    output.html_safe
+  end
+
 end
