c3ae1f4039c9000f888463b3d9f37b09fc37c626
Rachel Rice <rachel.rice@gmail.com>
2013-07-03 22:20:36 +0100

    Commiting any uncommited files before pusing repository

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,8 @@
 class Reservation < ActiveRecord::Base
 
+  belongs_to :user
+  belongs_to :device
+
   attr_accessor :start_at_time, :start_at_date, :end_at_time, :end_at_date
 
   def initialize(options={})
@@ -8,5 +11,9 @@ class Reservation < ActiveRecord::Base
     self.start_at = Time.zone.parse("#{start_at_date} #{start_at_time}") if start_at_date && start_at_time
     self.end_at = Time.zone.parse("#{end_at_date} #{end_at_time}") if end_at_date && end_at_time
   end
+
+
+  ## TODO: validations
+  validates :user_id, :presence => true
 
 end
