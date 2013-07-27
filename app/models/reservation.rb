c490a58fefd5272a46f401789f11702f9bcecbee
Rachel Rice <rachel.rice@gmail.com>
2013-07-27 20:32:45 +0100

    Adding validation for overlapping reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,8 +12,25 @@ class Reservation < ActiveRecord::Base
     self.end_at = Time.zone.parse("#{end_at_date} #{end_at_time}") if end_at_date && end_at_time
   end
 
+  def reservation_overlap
+
+  reservations = reservation.where(:date => Date.today).where(:device_id => device_id)
+  overlaps = false
+
+    reservations.each do |reservation|
+    overlaps = true if reservation.overlaps_with?(self)
+    end
+
+   if overlaps
+    self.errors.add_to_base("reservation overlaps with another")
+  end
+end
+
+
+
 
   ## TODO: validations
   validates :user_id, :presence => true
+  validates :device_id, :reservation_overlap => false
 
 end
